64910edf7f90574f01cd890f88453c6aa9f246ab
Jack Li <jacky984479215@outlook.com>
2022-12-01 00:55:18 -0600

    add cors to flask

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from ..haystack.pipeline.pipeline import query
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/qna")
 def qna():
